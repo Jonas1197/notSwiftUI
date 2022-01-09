00d6fc1c8fad2a2f01a7ac952f5cf0cb2b9a59fb
Jonas Gamburg <jonasgamburg97@gmail.com>
2022-01-09 13:08:38 +0200

    Moved @discardableResult to implementation.

--- Sources/NotSwiftUI/Objectified.swift
+++ Sources/NotSwiftUI/Objectified.swift
@@ -12,53 +12,53 @@ public typealias FinishedAnimationComplition = (Bool) -> Void
 
 public protocol Objectified: UIResponder, NSCoding, UIAppearance, UIAppearanceContainer, UIDynamicItem, UITraitEnvironment, UICoordinateSpace, UIFocusItem, UIFocusItemContainer, CALayerDelegate {
     
-    @discardableResult func shadowed(with color: UIColor, offset: CGSize, radius: CGFloat, _ opacity: Float) -> UIView
-    @discardableResult func translucent(_ opacity: Float)            -> UIView
-    @discardableResult func invisible()                              -> UIView
-    @discardableResult func visible()                                -> UIView
-    @discardableResult func backgroundColored(_ color: UIColor)      -> UIView
-    @discardableResult func tintColored(_ color: UIColor)            -> UIView
-    @discardableResult func hidden()                                 -> UIView
-    @discardableResult func showing()                                -> UIView
-    @discardableResult func rounded(_ radius: CGFloat)               -> UIView
-    @discardableResult func bordered(width: CGFloat, color: UIColor) -> UIView
-    @discardableResult func setSystemFont(with size: CGFloat)        -> UIView
-    @discardableResult func setText(_ text: String)                  -> UIView
-    @discardableResult func setImage(imageName: String)              -> UIView
-    @discardableResult func setImage(image: UIImage?)                -> UIView
+    func shadowed(with color: UIColor, offset: CGSize, radius: CGFloat, _ opacity: Float) -> UIView
+    func translucent(_ opacity: Float)            -> UIView
+    func invisible()                              -> UIView
+    func visible()                                -> UIView 
+    func backgroundColored(_ color: UIColor)      -> UIView
+    func tintColored(_ color: UIColor)            -> UIView
+    func hidden()                                 -> UIView
+    func showing()                                -> UIView
+    func rounded(_ radius: CGFloat)               -> UIView
+    func bordered(width: CGFloat, color: UIColor) -> UIView
+    func setSystemFont(with size: CGFloat)        -> UIView
+    func setText(_ text: String)                  -> UIView
+    func setImage(imageName: String)              -> UIView
+    func setImage(image: UIImage?)                -> UIView
     
     /**
      Determines whether subviews are confined to the bounds of the view - sets 'true' by default.
      */
-    @discardableResult func clipsedToBounds(_ clipsed: Bool)         -> UIView
+    func clipsedToBounds(_ clipsed: Bool)         -> UIView
     
-    @discardableResult func setSymbol(symbolName: String,
+    func setSymbol(symbolName: String,
                    pointSize: CGFloat,
                    contentMode: UIImageView.ContentMode,
                    weight: UIImage.SymbolWeight,
                    scale: UIImage.SymbolScale,
                    tintColor: UIColor) -> UIView
     
-    @discardableResult func centered(in view: UIView) -> UIView
+    func centered(in view: UIView) -> UIView
     
-    @discardableResult func filled(in view: UIView,
+    func filled(in view: UIView,
                      leadingPadding:  CGFloat,
                      trailingPadding: CGFloat,
                      topPadding:      CGFloat,
                      bottomPadding:   CGFloat) -> UIView
     
-    @discardableResult func filled(in view: UIView,
+    func filled(in view: UIView,
                 bellow topView: UIView,
                 withPadding padding: CGFloat) -> UIView
   
-    @discardableResult func animated(withDuration duration: TimeInterval,
+    func animated(withDuration duration: TimeInterval,
                         andDelay delay: TimeInterval,
                         withOptions options: UIView.AnimationOptions,
                         _ completion: @escaping AnimationComplition,
                         _ endAnimationComplition: FinishedAnimationComplition?) -> UIView
     
-    @discardableResult func fonted(ofType type: FontType, size: CGFloat, weight: UIFont.Weight) -> UIView
+    func fonted(ofType type: FontType, size: CGFloat, weight: UIFont.Weight) -> UIView
     
     
-    @discardableResult func chain(with action: ChainedAction) -> UIView
+    func chain(with action: ChainedAction) -> UIView
 }
