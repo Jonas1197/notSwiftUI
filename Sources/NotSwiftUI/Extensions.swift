65f48dd9cbc37dec5711d6fad40aa0dcf8e90b88
Jonas Gamburg <jonasgamburg97@gmail.com>
2022-04-13 16:43:44 +0300

    Added shake animation.

--- Sources/NotSwiftUI/Extensions.swift
+++ Sources/NotSwiftUI/Extensions.swift
@@ -621,6 +621,7 @@ extension UIView {
         return self
     }
     
+    //MARK: - Useful animations
     @discardableResult public func actionWithSpringAnimation(_ action: @escaping () -> Void) -> UIView {
         UIView.animate(withDuration: 0.15, delay: 0, options: .allowUserInteraction) {
             self.transform = .init(scaleX: 0.94, y: 0.94)
@@ -633,6 +634,16 @@ extension UIView {
                 action()
             }
         }
+        
+        return self
+    }
+    
+    @discardableResult public func shake() -> UIView {
+        let animation = CAKeyframeAnimation(keyPath: "transform.translation.x")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        animation.duration = 0.6
+        animation.values = [-20.0, 20.0, -20.0, 20.0, -10.0, 10.0, -5.0, 5.0, 0.0 ]
+        layer.add(animation, forKey: "shake")
         
         return self
     }
